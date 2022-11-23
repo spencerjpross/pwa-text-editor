868c63a70387671b4e89dccce0505e235827acd0
Spencer Ross <spencerjpross@gmail.com>
2022-11-23 10:13:31 -0700

    moved folders to root file

---
+++ client/src/js/database.js
@@ -0,0 +1,36 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  const textEditorDb = await openDB('jate', 1);
+  const tx = textEditorDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({id: 1, value: content});
+  const result = await request;
+  console.log(result.value);
+};
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  const textEditorDb = await openDB('jate', 1);
+  const tx = textEditorDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  console.log(result.value);
+  return result.value;
+}
+
+initdb();
